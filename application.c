f57ec4ff1e2a6d99fc765416d35f0b409d96d5f5
wangyonglin <admin@wangyonglin.com>
2022-05-22 02:01:34 -0400

    20220522

--- application.c
+++ application.c
@@ -1,8 +1,6 @@
-#include <wangyonglin/linux.h>
 #include <wangyonglin/config.h>
-#include <wangyonglin/daemon.h>
 #include <wangyonglin/log.h>
-#include <wangyonglin/pid.h>
+
 
 ok_t application(config_t *config,void *args){
     log_write(config,LOG_INFO,"application ok");
